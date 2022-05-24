dbf3c962ee095eb9d2a98308f9556f9fb7272d62
Jessica <jsisavath@gmail.com>
2022-05-24 01:11:32 -0500

    trying to modularize routes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const router = require('express').Router();
 
+
+//sends notes to the notes.html file
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+//sends to homepage if path issue
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,31 @@ const express = require('express');
 //instantiate the serve and tell it to listen for requests
 const app = express();
 
-const {  } = require('./data/animals');
+const database = require ('./db/db.json');
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//when Heroku runs our app it sets an environment variable called process.env.PORT, tells app to use that port if its set and if not
+//default to port 3001
+const PORT = process.env.PORT || 3001;
+
+// This sets up data parsing-- Express will interpret it/format data as JSON.
+// This is required for API calls!
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//add routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(database);
+  });
+
+//replaced hard code with port variable
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
