acd7c828fdf9eeb7b0e5792864b4361e5f077583
Jessica <jsisavath@gmail.com>
2022-05-25 15:28:40 -0500

    edited code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,27 +2,6 @@ const router = require('express').Router();
 const fs = require('fs');
 const savedNotes = require ('../../db/db.json');
 
-//get/api/notes should read db.json file and return all saved notes as json
-router.get('/api/notes', (req, res) => {
-        let results=oldNotes
-        res.json(results)
-    });
-
-function findById(id, noteArray) {
-        const result = noteArray.filter((note) => note.id === id)[0];
-        return result;
-      }
-    
-router.get('/api/notes/:id', (req, res) => {
-        const result = findById(req.params.id, savedNotes);
-        if (result) {
-          res.json(result);
-        } else {
-          res.send(404);
-        }
-      });
-
-
 function createNewNote (body, notesArray) { 
     const newNote= body;
     f (!Array.isArray(notesArray))
@@ -40,18 +19,13 @@ function createNewNote (body, notesArray) {
         JSON.stringify (notesArray), null,2);
 
     return newNote;
-};
+}
 
-// posting note function route receives a new note to save on request body, then adds it to the json file,
-//then returns it to the client 
-router.post('/api/notes', (req, res) => {
-    req.body.id = oldNotes.length.toString();
-    // req.body is where our incoming content will be
-    const oldNotes = createNewNote(req.body, oldNotes);
-  
-    res.json(notexs);
-    
-  
+function findById(id, noteArray) {
+    const result = noteArray.filter((note) => note.id === id)[0];
+    return result;
+}
+
 function deleteNote(id, notesArray) {
     for (let i = 0; i < notesArray.length; i++) {
       let note = notesArray[i];
@@ -67,6 +41,23 @@ function deleteNote(id, notesArray) {
       }
     }
   }
+
+//get/api/notes should read db.json file and return all saved notes as json
+router.get('/api/notes', (req, res) => {
+        let results=oldNotes
+        res.json(results)
+    });
+
+    
+router.get('/api/notes/:id', (req, res) => {
+        const result = findById(req.params.id, savedNotes);
+        if (result) {
+          res.json(result);
+        } else {
+          res.send(404);
+        }
+      });
+
   
 router.delete('/api/notes/:id', (req, res) => {
     deleteNote(req.params.id, savedNotes);
