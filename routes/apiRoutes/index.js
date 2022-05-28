c3e921cb925427c597a8ad9304c094d7eccb07ef
Jessica <jsisavath@gmail.com>
2022-05-27 21:56:47 -0500

    added lib folder

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,69 +1,34 @@
 const router = require('express').Router();
-const fs = require('fs');
-const savedNotes = require ('../../db/db.json');
+const {savedNotes} = require ('../../db/db.json');
+const {findById, createNewNote, deleteNote } = require('../../lib/notes');
 
-function createNewNote (body, notesArray) { 
-    const newNote= body;
-    f (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
 
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname,'../..db/db.json'),
-        JSON.stringify (notesArray), null,2);
-
-    return newNote;
-}
-
-function findById(id, noteArray) {
-    const result = noteArray.filter((note) => note.id === id)[0];
-    return result;
-}
-
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-      let note = notesArray[i];
-  
-      if (note.id == id) {
-        notesArray.splice(i, 1);
-        fs.writeFileSync(
-          path.join(__dirname, '../../db.json'),
-          JSON.stringify(notesArray, null, 2)
-        );
-  
-        break;
-      }
-    }
-  }
+ router.get('/api/notes/:id', (req, res) => {
+          const result = findById(req.params.id, savedNotes);
+          if (result) {
+            res.json(result);
+          } else {
+            res.send(404);
+          }
+        });
 
 //get/api/notes should read db.json file and return all saved notes as json
 router.get('/api/notes', (req, res) => {
-        let results=oldNotes
-        res.json(results)
+  let results= savedNotes;
+  res.json(results);
+});
+
+router.post('/api/notes', (req,res) => {
+  // set id based on what the next index of the array will be
+        req.body.id = savedNotes.length.toString();
+        // if any data in req.body is incorrect, send 400 error back
+        const noteBar= createNewNote(req.body, savedNotes);
+        res.json(noteBar);
     });
 
-    
-router.get('/api/notes/:id', (req, res) => {
-        const result = findById(req.params.id, savedNotes);
-        if (result) {
-          res.json(result);
-        } else {
-          res.send(404);
-        }
-      });
-
-  
 router.delete('/api/notes/:id', (req, res) => {
     deleteNote(req.params.id, savedNotes);
     res.json(true);
 });
-
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 //require express.js 
 const express = require('express');
-//instantiate the server and tell it to listen for requests
-const app = express();
-
-
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 //when Heroku runs our app it sets an environment variable called process.env.PORT, tells app to use that port if its set and if not
 //default to port 3001
 const PORT = process.env.PORT || 3001;
+//instantiate the server and tell it to listen for requests
+const app = express();
+
 
 // This sets up data parsing-- Express will interpret it/format data as JSON.
 // This is required for API calls!
 //middleware
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-//add routes
+//use api routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-
-
 
 
 //replaced hard code with port variable
